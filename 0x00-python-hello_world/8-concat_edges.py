87384a40f9dd4b4bc9328e35b7b62a22c5c0d24e
wanguya <wanguyajay@gmail.com>
2023-01-15 07:16:12 -0800

    pin8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
+str = str[39:65] + str[107:112] + str[0:7]
+print(str)
